e1defa1ec6ad3857dfa7cfbd1848a3c4e15b7d65
clement-ghn <ent.clementgehin@gmail.com>
2022-02-24 11:01:35 -0500

    Mise à jour des dossiers

--- src/Pendu_gehin_dematini.py
+++ src/Pendu_gehin_dematini.py
@@ -14,29 +14,29 @@ window.minsize(360, 270)#taille minimale de la fenetre
 window.geometry("780x540")#taille de la fenetre qui apparait
 window.config(background='#FFA91C')
 
-frame = Frame(window, bg='#FFA91C')#creation de la frame
+#frame = Frame(window, bg='#FFA91C')#creation de la frame
 #premiere texte dans la fenetre
-pendu_title = Label(window, text="Bienvenue dans le jeu du pendu !", font =("Lato", 20), bg='#FFA91C')
-pendu_title.pack(expand=YES )#position du texte
+#pendu_title = Label(window, text="Bienvenue dans le jeu du pendu !", font =("Lato", 20), bg='#FFA91C')
+#pendu_title.pack(expand=YES )#position du texte
 
 #Deuxieme texte
-pendu_subtitle = Label(window, text="Choisissez votre mode de jeu :", font =("Lato", 15), bg='#FFA91C', fg='white')
-pendu_subtitle.pack(expand=YES)
+#pendu_subtitle = Label(window, text="Choisissez votre mode de jeu :", font =("Lato", 15), bg='#FFA91C', fg='white')
+#pendu_subtitle.pack(expand=YES)
 
 #Bouton 1 joueur
-joueursolo_button = Button(frame, text="1 joueur", font=("Lato", 18), bg='#FFA91C', fg='white',)
-joueursolo_button.pack()
+#joueursolo_button = Button(frame, text="1 joueur", font=("Lato", 18), bg='#FFA91C', fg='white',)
+#joueursolo_button.pack()
 
-frame.pack(expand=YES)
+#frame.pack(expand=YES)
 
 #Bouton 2 joueurs
-joueurmulti_button = Button(frame, text="2 joueurs", font=("Lato", 18), bg='#FFA91C', fg='white',)
-joueurmulti_button.pack()
+#joueurmulti_button = Button(frame, text="2 joueurs", font=("Lato", 18), bg='#FFA91C', fg='white',)
+#joueurmulti_button.pack()
 
-frame.pack(expand=YES)
+#frame.pack(expand=YES)
 
 
-window.mainloop()#afficher la fenetre
+#window.mainloop()#afficher la fenetre
 def joueur_1() :
     mots = ["arrosoir", "aristocrate", "attentat", "action","abricot", "alentour", "analphabète", "alcool", "asticot", "animal", "bricolage","brebis", "bouton", "blanchir" "beurre", "boulette", "bélier","bleu", "blanchisserie","bourreau", "bureau", "collant" "cigarette", "cavalier", "carreau", "confinement", "coupure", "couteau", "clafoutis", "caramel", "coupable", "dessin", "desirable", "destin", "distant", "dedoubler", "discuter", "discours", "divin", "deposer", "décorer", "embeter", "enivrer", "esclave", "eprouver", "emballage", "epurer", "esperer", "evident", "essuyer", "effacer", "finale", "forteresse", "fortifier", "fouler", "finir", "fondant", "flouter", "fenouille", "festin", "futur", "gastronomie", "gaufre", "gouverner", "girafe", "gestion", "gouter", "grammaire", "gagner", "gourmand", "galactique", "habitation", "habit", "hippocampe", "hache", "haleine","halte", "hallucinant","hameconner", "handicap", "harceler", "ignoble", "irresistible", "inhabitable", "inespere", "irrespect", "illogique", "inoui", "interieur","inspecteur", "idiot", "impossible", "joie", "jouet", "juste", "jolie", "jouer", "jeu", "jouable", "jacuzzi", "jaloux", "jambe", "kiwi", "kayak", "karate", "karma", "kebab", "kangourou", "katana", "kayakiste", "kart", "karaoke", "lion", "livre", "lecture", "lycee", "lourd", "leger", "lisser", "lymphe", "laver", "larve", "mais", "mystere", "miserable", "mignon", "miroir", "mouton", "mangue", "mozambie", "moustique", "merveilleux", "nouveau", "nouvelle", "neon", "naissance", "novice", "noyau", "noyer", "nounours", "noeud", "nuage", "osculter", "ouvrir", "obese", "obligation", "obligatoire", "omelette", "oublier", "ours", "obstacle", "offrir", "pleuvoir", "pluie", "poussiere", "pousser", "pleurer", "pistolet", "poubelle", "promettre", "poussette", "pneu", "quiproquo", "qualifier", "quitter", "quittance", "quarantaine", "quiche", "qualite", "quartz", "quarante", "quart", "rateau", "rester", "redoutable", "rire", "regle", "risible", "rougir", "rouge", "rigoler", "retenir", "salive", "sable","saluer", "singe", "songe", "signer", "souffler", "sourire", "souris", "the", "tomate", "tisane", "trophee", "terrain", "tente", "tante", "tirer", "trou", "ulcere", "ultime", "ultra", "urbanisme", "un", "urgence", "usurpation", "utile", "utilite", "use", "violet", "voler", "volet", "voir", "venir", "viande", "vilain", "vacarme", "ver", "verre", "wagon", "web","wifi", "xylophone", "zizanie", "zone", "zoo"] 
     #on crée un dictionnaire de mots
